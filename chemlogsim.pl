8dec100deaf7fcd04611d1260cc663ed2f0c5d89
Alexandre Martins <alexmsmartins@gmail.com>
2012-07-20 19:23:56 +0100

    Code now works with systems of equations. Namely the system with an unidirectional reaction of the form:  A -> B is represented in euler_unid_reaction.pl.

--- chemlogsim.pl
+++ chemlogsim.pl
@@ -1,6 +1,12 @@
 :-
   % get the differential equations
   % apply euler's method
+  [euler_systems_ODE],
+  euler(100,0 , 0, 10, 200,X1_ts, X2_ts),
   % save results to file
+  tell('concentrations.csv'),
+  write(xxx),
+  told,
+  
   % execute gnuplot
   shell("gnuplot -p plot.plt").
---
+++ euler_systems_ODE.pl
@@ -0,0 +1,29 @@
+euler(X1_0, X2_0, T_0, H, Stop, X1_ret, X2_ret) :-
+  euler_step(X1_0, X2_0, T_0, H, Stop, [], [], X1_ret, X2_ret).
+
+euler_step(X1_n, X2_n, T_n, H, Stop, X1_list, X2_list, [X1_n|X1_ret], [X2_n|X2_ret]) :-
+  T_n < Stop,
+  write(T_n),tab(2), write(X1_n),tab(2), write(X2_n), nl,
+
+  dX1(T_n, X1_n, X2_n, DX1_n),
+  X1_n1 is X1_n + H * DX1_n,
+
+  dX2(T_n, X1_n, X2_n, DX2_n),
+  X2_n1 is X2_n + H * DX2_n,
+
+  T_n1 is T_n + H,
+  euler_step(X1_n1, X2_n1, T_n1, H, Stop, X1_list, X2_list, X1_ret, X2_ret).
+  
+euler_step(X1_n, X2_n, H, T_n, Stop, X1_list, X2_list, [], []) :-
+  T_n >= Stop.
+
+%  [diff(X1) :- -2.2 * X1^2, diff(X2) :- 2.2*X1^2],
+%  X_n1 is X_n + H*(Func(T, X_n)).
+
+% Generalize this to include a system of ODEs
+dX1(Time, X1, X2, DX1) :- 
+  DX1 = -A * X1, A = 0.02.
+
+dX2(Time, X1, X2, DX2) :- 
+  DX2 = A * X1, A = 0.02.
+
